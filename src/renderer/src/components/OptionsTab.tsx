4329697c2e16ba5ed7f0623fca741d60bb53f659
Strahinja <111398879+Strahinja2112@users.noreply.github.com>
2024-01-26 22:52:47 +0100

    zzz

--- src/renderer/src/components/OptionsTab.tsx
+++ src/renderer/src/components/OptionsTab.tsx
@@ -1,7 +1,13 @@
 import { useNotes } from "@renderer/store/useNotes";
-import { File, Folder, Plus, RotateCcw, Settings, Trash } from "lucide-react";
+import { FileIcon, FolderIcon, PlusIcon, RotateCcw, Settings, Trash } from "lucide-react";
 import { Button } from "./ui/button";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 type Props = {};
@@ -12,39 +18,26 @@ export default function OptionsTab({}: Props) {
   return (
     <div
       className="w-full h-[35px] border-t flex items-center justify-between px-1"
-      onClick={(e) => e.stopPropagation()}
+      onClick={(e) => e.stopPropagation}
     >
       <div className="flex items-center justify-center gap-1">
-        <Popover>
-          <PopoverTrigger asChild>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <Button size="mini">
-              <Plus className="w-5 h-5 text-zinc-300" />
+              <PlusIcon className="h-4 w-4" />
             </Button>
-          </PopoverTrigger>
-          <PopoverContent className="z-[100] flex flex-col p-1 w-[100px] bg-secondary drop-shadow-lg gap-1 text-muted-foreground">
-            <span className="text-center text-sm">Add new: </span>
-            <Button
-              size="mini"
-              className="flex justify-between"
-              style={{
-                paddingInline: "12px"
-              }}
-            >
-              <Folder className="w-5 h-5 text-zinc-300" />
-              Folder
-            </Button>
-            <Button
-              size="mini"
-              style={{
-                padding: "12px"
-              }}
-              onClick={onCreate}
-            >
-              <File className="w-5 h-5 text-zinc-300" />
-              Note
-            </Button>
-          </PopoverContent>
-        </Popover>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[120px] z-[100] bg-secondary">
+            <DropdownMenuItem onClick={onCreate}>
+              <FileIcon className="h-4 w-4 mr-2" />
+              New File
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <FolderIcon className="h-4 w-4 mr-2" />
+              New Folder
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Button onClick={onDelete} size="mini" disabled={!selectedNote}>
           <Trash className="w-4 h-4 text-zinc-300" />
         </Button>
